2e2781807c80eb512dbd2da2287781361b0e27bc
Mustafa Albassam <Mustafa.s.albassam@gmail.com>
2019-06-06 10:19:51 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen"; 
-var cardTwo = "queen"; 
-var cardThree = "king";
-var cardFour = "king"; 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = 'queen'; 
+cardsInPlay.push('cardOne'); 
+console.log("User flipped queen");
+var cardTwo = 'king'; 
+cardsInPlay.push('cardTwo');
+console.log('User flipped king');
+
+if (cardsInPlay.length === 2){ 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	result = alert("You found a match!"); 
+}
+	else {
+	result = alert("Sorry try again!");
+}
+}
+
